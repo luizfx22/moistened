74cfbd219f8ef6ce01dc4605feea575cae9627da
iLuiizUHD <luizgomes749@gmail.com>
2021-07-19 12:07:44 -0300

    Added some stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: ['prettier'],
-  // add your custom rules here
-  rules: {},
-}
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	parserOptions: {
+		parser: "babel-eslint",
+	},
+	extends: [
+		"@nuxtjs",
+		"prettier",
+		"prettier/vue",
+		"plugin:prettier/recommended",
+		"plugin:nuxt/recommended",
+		"plugin:@typescript-eslint/eslint-recommended",
+		"plugin:@typescript-eslint/recommended",
+	],
+	plugins: ["@typescript-eslint", "prettier"],
+	rules: {
+		"prettier/prettier": "error",
+	},
+};
--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,17 @@
-module.exports = (req, res) => {
-  const { name = 'World' } = req.query;
-  res.status(200).send(`Hello ${name}!`);
-};
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+
+const app = express();
+
+// Use
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.get("/api", (req, res) => {
+	res.status(200).json({ message: "It works!" });
+});
+
+export default app;
