3ec012fc57f07a06d357b127cb0102c84370c53b
iLuiizUHD <luizgomes749@gmail.com>
2021-07-22 20:54:50 -0300

    Menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
 	<v-app>
-		<v-navigation-drawer v-model="drawer" fixed app src="https://picsum.photos/1920/1080">
+		<v-navigation-drawer v-model="drawer" fixed absolute app src="https://picsum.photos/1920/1080">
 			<v-list class="pt-0">
 				<v-list-item style="background-color: #15151555">
 					<v-list-item-action>
@@ -29,6 +29,38 @@
 		<v-app-bar fixed app>
 			<v-app-bar-nav-icon class="ml-2" @click.stop="drawer = !drawer" />
 			<v-toolbar-title>{{ $utils.capitalize($route.name) }}</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-menu
+				transition="slide-x-transition"
+				bottom
+				min-width="200px"
+				class="mr-5"
+				rounded
+				offset-y
+			>
+				<template #activator="{ on }">
+					<v-btn icon x-large v-on="on">
+						<v-avatar color="brown" size="48">
+							<span class="white--text text-h5">JJ</span>
+						</v-avatar>
+					</v-btn>
+				</template>
+				<v-card>
+					<v-list-item-content class="justify-center">
+						<div class="mx-auto text-center">
+							<v-avatar color="brown">
+								<span class="white--text text-h5">JJ</span>
+							</v-avatar>
+							<h3>MEU PINTO</h3>
+							<p class="text-caption mt-1">putasnaescola.com.br</p>
+							<v-divider class="my-3"></v-divider>
+							<v-btn depressed rounded text> Edit Account </v-btn>
+							<v-divider class="my-3"></v-divider>
+							<v-btn depressed rounded text> Disconnect </v-btn>
+						</div>
+					</v-list-item-content>
+				</v-card>
+			</v-menu>
 		</v-app-bar>
 		<v-main>
 			<v-container>
