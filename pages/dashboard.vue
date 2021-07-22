d090610c22529f43b0856285ecd29a5d6e584810
iLuiizUHD <luizgomes749@gmail.com>
2021-07-21 21:48:27 -0300

    Exemplo de uso do componente Chart

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,12 +1,43 @@
 <template>
-	<div></div>
+	<div>
+		<Chart :chart-data="chartData"></Chart>
+	</div>
 </template>
 
 <script>
 export default {
 	name: "Dashboard",
 	data() {
-		return {};
+		return {
+			chartData: {
+				labels: [
+					"Janeiro",
+					"Fevereiro",
+					"Março",
+					"Abril",
+					"Maio",
+					"Junho",
+					"Julho",
+					"Agosto",
+					"Setembro",
+					"Outubro",
+					"Novembro",
+					"Dezembro",
+				],
+				datasets: [
+					{
+						label: "Data One",
+						backgroundColor: "#f87979",
+						data: [100, 125, 55, 69, 24, 110, 445, 321, 111, 266, 122, 110],
+					},
+					{
+						label: "Data two",
+						backgroundColor: "#A86669",
+						data: [100.4, 65, 95, 119.1, 174, 260, 295, 421, 31, 186, 222, 30],
+					},
+				],
+			},
+		};
 	},
 };
 </script>
