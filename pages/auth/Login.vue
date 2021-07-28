3b6ac613a0f6f1a8913246ce2cc8c388352661a1
iLuiizUHD <luizgomes749@gmail.com>
2021-07-27 23:04:15 -0300

    Criação tela de registro

--- pages/auth/Login.vue
+++ pages/auth/Login.vue
@@ -1,116 +1,114 @@
 <template>
-	<div>
-		<v-card class="mx-auto" max-width="500" :loading="cardLoading">
-			<v-card-title primary-title> Moistened </v-card-title>
-			<v-card-text class="pb-0">
-				<v-form ref="loginForm" v-model="isFormValid">
-					<v-container>
-						<v-row>
-							<v-col>
-								<p class="mb-2">
-									<b>E-mail</b>
-								</p>
-								<v-text-field
-									ref="emailField"
-									v-model="form.email"
-									:disabled="cardLoading"
-									:rules="rules.emailRules"
-									required
-									elevation="0"
-									solo
-									outlined
-								></v-text-field>
-							</v-col>
-						</v-row>
-						<v-row>
-							<v-col>
-								<div class="mb-2 flex-row justify-space-between" style="display: flex">
-									<p style="margin: 0">
-										<b>Senha</b>
-									</p>
-									<p style="margin: 0">
-										<b>Esqueceu sua senha?</b>
-									</p>
-								</div>
-								<v-text-field
-									ref="senhaField"
-									v-model="form.senha"
-									:disabled="cardLoading"
-									:append-icon="mostrarSenha ? 'mdi-eye' : 'mdi-eye-off'"
-									:rules="rules.passwordRules"
-									:type="mostrarSenha ? 'text' : 'password'"
-									name="input-10-1"
-									hint="Pelo menos 8 caracteres!"
-									counter
-									outlined
-									elevation="0"
-									solo
-									@click:append="mostrarSenha = !mostrarSenha"
-									@keypress.enter.native="handleLoginWithEmail"
-								></v-text-field>
-							</v-col>
-						</v-row>
-					</v-container>
-				</v-form>
-			</v-card-text>
-			<v-divider></v-divider>
-			<v-card-actions>
+	<v-card class="mx-auto" max-width="500" :loading="cardLoading" light>
+		<v-card-title primary-title> Moistened </v-card-title>
+		<v-card-text class="pb-0">
+			<v-form ref="loginForm" v-model="isFormValid">
 				<v-container>
 					<v-row>
-						<v-col class="d-flex justify-center">
-							<v-btn
-								elevation="2"
-								color="success"
-								:loading="cardLoading"
-								@click="handleLoginWithEmail"
-							>
-								<v-icon>mdi-login</v-icon>
-								<span class="pl-1">Acessar sistema</span>
-							</v-btn>
+						<v-col>
+							<p class="mb-2">
+								<b>E-mail</b>
+							</p>
+							<v-text-field
+								ref="emailField"
+								v-model="form.email"
+								:disabled="cardLoading"
+								:rules="rules.emailRules"
+								required
+								elevation="0"
+								solo
+								outlined
+							></v-text-field>
 						</v-col>
 					</v-row>
 					<v-row>
 						<v-col>
-							<v-divider></v-divider>
-						</v-col>
-						<v-col cols="1" class="ma-0 pa-0 d-flex justify-center">
-							<span style="font-size: 14px; margin-top: 2px">OU</span>
-						</v-col>
-						<v-col>
-							<v-divider></v-divider>
-						</v-col>
-					</v-row>
-					<v-row align-content="center">
-						<v-col class="d-flex justify-center">
-							<v-btn
-								color="#f9fafb"
-								elevation="2"
-								:loading="googleLoading"
-								:disabled="googleLoading"
-								@click="handleLoginWithGoogle"
-							>
-								<img class="google-logo" src="/Google__G__Logo.svg" alt="Google logo" />
-								<span style="color: black">Entrar com o Google</span>
-							</v-btn>
-						</v-col>
-					</v-row>
-					<v-row>
-						<v-col>
-							<v-divider></v-divider>
-						</v-col>
-					</v-row>
-					<v-row>
-						<v-col class="d-flex justify-center">
-							<v-btn elevation="2" color="blue darken-1" dark>
-								<v-icon>mdi-account-plus</v-icon>
-								<span class="pl-1">Criar uma nova conta</span>
-							</v-btn>
+							<div class="mb-2 flex-row justify-space-between" style="display: flex">
+								<p style="margin: 0">
+									<b>Senha</b>
+								</p>
+								<p style="margin: 0">
+									<b>Esqueceu sua senha?</b>
+								</p>
+							</div>
+							<v-text-field
+								ref="senhaField"
+								v-model="form.senha"
+								:disabled="cardLoading"
+								:append-icon="mostrarSenha ? 'mdi-eye' : 'mdi-eye-off'"
+								:rules="rules.passwordRules"
+								:type="mostrarSenha ? 'text' : 'password'"
+								name="input-10-1"
+								hint="Pelo menos 8 caracteres!"
+								counter
+								outlined
+								elevation="0"
+								solo
+								@click:append="mostrarSenha = !mostrarSenha"
+								@keypress.enter.native="handleLoginWithEmail"
+							></v-text-field>
 						</v-col>
 					</v-row>
 				</v-container>
-			</v-card-actions>
-		</v-card>
-	</div>
+			</v-form>
+		</v-card-text>
+		<v-divider></v-divider>
+		<v-card-actions>
+			<v-container>
+				<v-row>
+					<v-col class="d-flex justify-center">
+						<v-btn
+							elevation="2"
+							color="success"
+							:loading="cardLoading"
+							@click="handleLoginWithEmail"
+						>
+							<v-icon>mdi-login</v-icon>
+							<span class="pl-1">Acessar sistema</span>
+						</v-btn>
+					</v-col>
+				</v-row>
+				<v-row>
+					<v-col>
+						<v-divider></v-divider>
+					</v-col>
+					<v-col cols="1" class="ma-0 pa-0 d-flex justify-center">
+						<span style="font-size: 14px; margin-top: 2px">OU</span>
+					</v-col>
+					<v-col>
+						<v-divider></v-divider>
+					</v-col>
+				</v-row>
+				<v-row align-content="center">
+					<v-col class="d-flex justify-center">
+						<v-btn
+							color="#f9fafb"
+							elevation="2"
+							:loading="googleLoading"
+							:disabled="googleLoading"
+							@click="handleLoginWithGoogle"
+						>
+							<img class="google-logo" src="/Google__G__Logo.svg" alt="Google logo" />
+							<span style="color: black">Entrar com o Google</span>
+						</v-btn>
+					</v-col>
+				</v-row>
+				<v-row>
+					<v-col>
+						<v-divider></v-divider>
+					</v-col>
+				</v-row>
+				<v-row>
+					<v-col class="d-flex justify-center">
+						<v-btn elevation="2" color="blue darken-1" dark nuxt to="/auth/register">
+							<v-icon>mdi-account-plus</v-icon>
+							<span class="pl-1">Criar uma nova conta</span>
+						</v-btn>
+					</v-col>
+				</v-row>
+			</v-container>
+		</v-card-actions>
+	</v-card>
 </template>
 
 <script>
@@ -153,9 +151,6 @@ export default {
 				passwordRules: [(v) => !!v || "Coloque uma senha!"],
 			};
 		},
-	},
-	created() {
-		this.$vuetify.theme.dark = false;
 	},
 	methods: {
 		async handleLoginWithEmail() {
