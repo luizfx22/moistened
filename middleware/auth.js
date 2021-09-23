f0adaab14a6d0f735ab3b4d554cd28ee66ab499f
AestheticSauce22 <luizgomes749@gmail.com>
2021-09-23 15:00:29 +0000

    Teste visualizar a sessão

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,8 @@
-export default function ({ app }) {
-	console.log(app.$supabase.auth?.session())
+export default async function ({ app }) {
+	const session = await app.$supabase.auth?.session();
+
+	console.log(session)
+
 	// if (app.$supabase.auth?.currentUser?.aud !== "authenticated") {
 	// 	return window.onNuxtReady(() => {
 	// 		window.$nuxt.$router.push("/auth/login");
