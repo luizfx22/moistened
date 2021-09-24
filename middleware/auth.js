bec95804d0918cdd75b86cd4c0d3a5e480792d11
iLuiizUHD <luizgomes749@gmail.com>
2021-09-24 20:58:59 -0300

    Início grupos

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,17 @@
 export default async function ({ app, redirect }) {
 	const session = await app.$supabase.auth.session();
 
-	if (!session) {
-		redirect("/auth/login");
-		return false;
-	}
+	// const { data, error } = app.$supabase
+	// 	.from("UsuariosGrupo")
+	// 	.select()
+	// 	.eq('usuario_id', session)
+
+	console.log(session, app.supabase);
+
+	// if (!session) {
+	// 	redirect("/auth/login");
+	// 	return false;
+	// }
 
 	return true;
 }
