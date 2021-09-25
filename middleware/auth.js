a38b6ab06f3df9ddeb47cf42a257ccf828def7eb
iLuiizUHD <luizgomes749@gmail.com>
2021-09-24 21:05:42 -0300

    Listar grupos do usuário

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,12 @@
 export default async function ({ app, redirect }) {
 	const session = await app.$supabase.auth.session();
 
-	// const { data, error } = app.$supabase
-	// 	.from("UsuariosGrupo")
-	// 	.select()
-	// 	.eq('usuario_id', session)
+	const { data, error } = app.$supabase
+		.from("UsuariosGrupo")
+		.select()
+		.eq("usuario_id", session.user.id);
 
-	console.log(session, app.supabase);
+	console.log(data, error);
 
 	// if (!session) {
 	// 	redirect("/auth/login");
