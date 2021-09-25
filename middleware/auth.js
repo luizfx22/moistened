b45f10355cba167d34f33ca6e3534a126d6b4446
iLuiizUHD <luizgomes749@gmail.com>
2021-09-24 21:28:08 -0300

    Fix: login

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,10 @@
 export default async function ({ app, redirect }) {
 	const session = await app.$supabase.auth.session();
 
-	const { data, error } = app.$supabase
-		.from("UsuariosGrupo")
-		.select("*")
-		.eq("usuario_id", session.user.id);
-
-	console.log(data, error);
-
-	// if (!session) {
-	// 	redirect("/auth/login");
-	// 	return false;
-	// }
+	if (!session) {
+		redirect("/auth/login");
+		return false;
+	}
 
 	return true;
 }
