7d7ace4acdd39f4e7538591a856a3a95546a7bde
Luiz <41802882+iLuiizUHD@users.noreply.github.com>
2021-10-04 11:06:57 -0300

    Novo componente para gráficos.
    Novo componente baseado no Apache eCharts. Na verdade é um wrapper para o eCharts

---
+++ components/e-charts.vue
@@ -0,0 +1,31 @@
+<template>
+	<div ref="chart-container" :style="{ width, height }"></div>
+</template>
+<script>
+import * as echarts from "echarts";
+
+export default {
+	props: {
+		options: {
+			type: Object,
+			required: true,
+		},
+
+		width: {
+			type: String,
+			default: "300px",
+		},
+
+		height: {
+			type: String,
+			default: "300px",
+		},
+	},
+
+	mounted() {
+		const chartDOM = this.$refs["chart-container"];
+		const chart = echarts.init(chartDOM);
+		chart.setOption(this.options);
+	},
+};
+</script>
