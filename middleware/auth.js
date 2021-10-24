4fddab56893726c03049a33eaeacf276daa24f42
iLuiizUHD <luizgomes749@gmail.com>
2021-10-24 17:56:58 -0300

    Re-Habilitando auitenticação

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
-export default function ({ app, redirect }) {
-	// export default async function ({ app, redirect }) {
-	// 	const session = await app.$supabase.auth.session();
+// export default function ({ app, redirect }) {
+export default async function ({ app, redirect }) {
+	const session = await app.$supabase.auth.session();
 
-	// 	if (!session) {
-	// 		redirect("/auth/login");
-	// 		return false;
-	// 	}
+	if (!session) {
+		redirect("/auth/login");
+		return false;
+	}
 
 	return true;
 }
