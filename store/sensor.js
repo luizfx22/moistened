80f7c61a0a328709a4a61204c4ba78c1507f396c
iLuiizUHD <luizgomes749@gmail.com>
2021-10-27 22:40:06 -0300

    Início vinculação sensor

--- store/sensor.js
+++ store/sensor.js
@@ -2,6 +2,10 @@ export const state = () => ({});
 
 export const mutations = {};
 
-export const actions = {};
+export const actions = {
+	async vincularSensorHome(store, codigo) {
+		const { data: sensor, error: sensorError } = await this.$supabase.from("Sensor");
+	},
+};
 
 export const getters = {};
