bd92a51914442c429929741bb7b96214adc8e1ca
Luiz <41802882+iLuiizUHD@users.noreply.github.com>
2021-12-01 20:17:47 -0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 // export default function ({ app, redirect }) {
 export default async function ({ app, redirect }) {
-	const session = await app.$supabase.auth.session();
+	const session = await app.$supabase?.auth?.session();
 
 	if (!session) {
 		redirect("/auth/login");
